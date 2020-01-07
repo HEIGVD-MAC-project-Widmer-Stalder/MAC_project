07d9c252febc08a314aecfba7429623a142fe25e
ncdm-stldr <ncdm.stldr@gmail.com>
2020-01-07 12:19:43 +0100

    adding deploy script

---
+++ telegram_bot/redeploy.sh
@@ -0,0 +1,7 @@
+git checkout master
+git pull origin master
+mvn clean install
+cd target
+pkill -9 telegram-bot-experiment-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar telegram-bot-experiment-1.0-SNAPSHOT-jar-with-dependencies.jar
+
